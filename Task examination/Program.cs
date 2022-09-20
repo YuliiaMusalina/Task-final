c90168d2191c34e13c903f1d0b34a3d24610a4fb
Ulia Kamalova <mysula@mail.ru>
2022-09-21 00:18:24 +0500

    Решение и проверка задания

---
+++ Task examination/Program.cs
@@ -0,0 +1,22 @@
+﻿string[] symbols = { "1234", "1567", "-2", "computer science" };
+string[] newSymbols = new string[symbols.Length];
+int count = 0;
+for (int i = 0; i < symbols.Length; i++)
+{
+    if (symbols[i].Length <= 3)
+    {
+        newSymbols[count] = symbols[i];
+        count++;
+    }
+
+}
+PrintArray(newSymbols);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
